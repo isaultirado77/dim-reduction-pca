372b0c7cc8906dd5d57fa74fccf08fe7b4fcac6b
Isaul <tiradolisaul@gmail.com>
2025-05-06 10:17:46 -0700

    init PCA analysis notebook (outline complete); PCA with sklearn notebook (in progress); PCA algorithm script (ready for implementation)

--- pca.py
+++ pca.py
@@ -1,1 +1,28 @@
 """PCA implementation from scratch."""
+
+import numpy as np
+
+
+
+def eig(): 
+    pass
+
+def projection_matrix(): 
+    pass
+
+class PCA: 
+    def __init__(self):
+        pass
+    
+    def fit(self): 
+        pass
+
+    def evaluate(self): 
+        pass
+
+
+def main(): 
+    pass
+
+if __name__ == '__main__': 
+    main()
